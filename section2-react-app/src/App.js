826abf617a27b4f2c10774efb099c26dbc0e2d69
ThoPN <thopn3@fpt.edu.vn>
2024-07-10 21:19:04 +0700

    Work data from back-end

--- section2-react-app/src/App.js
+++ section2-react-app/src/App.js
@@ -6,30 +6,11 @@ import { Footer } from './components/footer';
 import { Container, Row, Col } from "react-bootstrap";
 
 function App() {
-  let students = [
-    {
-      "id": 1,
-      "name": "Nguyen Tuan Anh",
-      "age": 30
-    },
-    {
-      "id": 2,
-      "name": "Pham Hoang Nguyen",
-      "age": 25
-    },
-    {
-      "id": 3,
-      "name": "Tran Hong Hanh",
-      "age": 21
-    }
-  ];
-
-  let classes = ["SE1831", "SE1832", "SE1833"];
-
+  
   return (
-    <Container fluid>
+    <Container>
       <Header />
-      <Content data1={students} data2={classes} />
+      <Content />
       <Footer />
     </Container>
   );
--- section2-react-app/src/components/content.js
+++ section2-react-app/src/components/content.js
@@ -1,6 +1,26 @@
+import { useEffect, useState } from "react";
 import { Col, Container, Row, Table } from "react-bootstrap";
+import {Link} from "react-router-dom";
 
-export default function Content({data1, data2}){
+export default function Content() {
+    const [students, setStudents] = useState([]);
+    const [classes, setClasses] = useState([]);
+
+    useEffect(() => {
+        // Call API at end-point: http://localhost:9999/students
+        fetch("http://localhost:9999/students")
+            .then(res => res.json())
+            .then(result => setStudents(result))
+            .catch(err => console.log(err));
+
+        // Call API at end-point: http://localhost:9999/classes
+        fetch("http://localhost:9999/classes")
+            .then(res => res.json())
+            .then(result => setClasses(result))
+            .catch(err => console.log(err));
+
+    }, []);
+
     return (
         <Row>
             <Col md={10}>
@@ -8,6 +28,11 @@ export default function Content({data1, data2}){
                     <Row>
                         <Col>
                             <h2>List of students</h2>
+                        </Col>
+                    </Row>
+                    <Row className="mt-3 mb-3">
+                        <Col>
+                            <a href="#">Add new a Student</a>
                         </Col>
                     </Row>
                     <Row>
@@ -18,15 +43,28 @@ export default function Content({data1, data2}){
                                         <th>Id</th>
                                         <th>Name</th>
                                         <th>Age</th>
+                                        <th>Class</th>
+                                        <th colSpan={2}>Function</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     {
-                                        data1?.map(s => (
+                                        students?.map(s => (
                                             <tr>
                                                 <td>{s?.id}</td>
                                                 <td>{s?.name}</td>
                                                 <td>{s?.age}</td>
+                                                <td>
+                                                    {
+                                                        classes?.find(c => c.id === s.class)?.name
+                                                    }
+                                                </td>
+                                                <td>
+                                                    <a href="#">Edit</a>
+                                                </td>
+                                                <td>
+                                                    <a href="#">Delete</a>
+                                                </td>
                                             </tr>
                                         ))
                                     }
@@ -36,7 +74,18 @@ export default function Content({data1, data2}){
                     </Row>
                 </Container>
             </Col>
-            <Col md={2}>Right content</Col>
+            <Col md={2}>
+                <h4>Classes</h4>
+                <ul>
+                    {
+                        classes?.map(c => 
+                            <li key={c.id} value={c.id}>
+                                <a href="#">{c.name}</a>
+                            </li>
+                        )
+                    }
+                </ul>
+            </Col>
         </Row>
     );
 }
